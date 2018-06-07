515df7aae46cbf007346db09218d99323c587960
Rene Stein <rene@renestein.net>
2018-06-07 18:53:06 +0200

    Fixed build

--- RXEventAggregator/Lib/EventProcessing/EventAggregator.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "stdafx.h"
-#include "EventAggregator.h"
-
-namespace RxEventAggregator
-{
-  namespace EventProcessing
-  {
-  }
-}
--- RXEventAggregator/Lib/EventProcessing/EventAggregator.h
+++ RXEventAggregator/Lib/EventProcessing/EventAggregator.h
@@ -3,58 +3,55 @@
 #include <type_traits>
 #include <rxcpp/rx.hpp>
 
-namespace RxEventAggregator
+namespace EventProcessing
 {
-  namespace EventProcessing
+  template <typename TBE>
+  class EventAggregator
   {
-    template <typename TBE>
-    class EventAggregator
-    {
-    public:
+  public:
 
-      EventAggregator();
+    EventAggregator();
 
-      EventAggregator(const EventAggregator& other) = delete;
-      EventAggregator(EventAggregator&& other) noexcept = delete;
-      EventAggregator& operator=(const EventAggregator& other) = delete;
-      EventAggregator& operator=(EventAggregator&& other) noexcept = delete;
-      ~EventAggregator() = default;
-      template <typename TE>
-      auto GetEventStream();
-      template <typename TE>
-      void PublishEvent(std::shared_ptr<TE> event);
-    private:
-      rxcpp::subjects::subject<std::shared_ptr<TBE>> _eventSubject;
-    };
-
-    template <typename TBE>
-    EventAggregator<TBE>::EventAggregator() : _eventSubject()
-    {
-    }
-
-    template <typename TBE>
+    EventAggregator(const EventAggregator& other) = delete;
+    EventAggregator(EventAggregator&& other) noexcept = delete;
+    EventAggregator& operator=(const EventAggregator& other) = delete;
+    EventAggregator& operator=(EventAggregator&& other) noexcept = delete;
+    ~EventAggregator() = default;
     template <typename TE>
-    auto EventAggregator<TBE>::GetEventStream()
-    {
-      static_assert(std::is_base_of_v<TBE, TE>, "TE should inherit from TBE");
-      return _eventSubject.get_observable()
-                          .map([](auto& baseEvent)
-                          {
-                            return std::dynamic_pointer_cast<TE>(baseEvent);
-                          })
-                          .filter([](auto& typedEvent)
-                          {
-                            return typedEvent.get() != nullptr;
-                          }).as_dynamic();
-    }
-
-    template <typename TBE>
+    auto GetEventStream();
     template <typename TE>
-    void EventAggregator<TBE>::PublishEvent(std::shared_ptr<TE> event)
-    {
-      static_assert(std::is_base_of_v<TBE, TE>, "TE should inherit from TBE");
-      _eventSubject.get_subscriber()
-                   .on_next(event);
-    }
+    void PublishEvent(std::shared_ptr<TE> event);
+  private:
+    rxcpp::subjects::subject<std::shared_ptr<TBE>> _eventSubject;
+  };
+
+  template <typename TBE>
+  EventAggregator<TBE>::EventAggregator() : _eventSubject()
+  {
+  }
+
+  template <typename TBE>
+  template <typename TE>
+  auto EventAggregator<TBE>::GetEventStream()
+  {
+    static_assert(std::is_base_of_v<TBE, TE>, "TE should inherit from TBE");
+    return _eventSubject.get_observable()
+                        .map([](auto& baseEvent)
+                        {
+                          return std::dynamic_pointer_cast<TE>(baseEvent);
+                        })
+                        .filter([](auto& typedEvent)
+                        {
+                          return typedEvent.get() != nullptr;
+                        }).as_dynamic();
+  }
+
+  template <typename TBE>
+  template <typename TE>
+  void EventAggregator<TBE>::PublishEvent(std::shared_ptr<TE> event)
+  {
+    static_assert(std::is_base_of_v<TBE, TE>, "TE should inherit from TBE");
+    _eventSubject.get_subscriber()
+                 .on_next(event);
   }
 }
