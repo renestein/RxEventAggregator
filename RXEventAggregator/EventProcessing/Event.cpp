69ef2b73cdadf79f70f9df10cacfe72eb6281fe2
Rene Stein <rene@renestein.net>
2018-06-07 08:59:31 +0200

    Added Event class

---
+++ RXEventAggregator/EventProcessing/Event.cpp
@@ -0,0 +1,31 @@
+﻿#include "stdafx.h"
+#include "Event.h"
+#include "../Misc/DateTime.h"
+using namespace std;
+using namespace Misc;
+namespace EventProcessing
+{
+  Event::Event(EventType eventType, std::string description) : m_eventType(eventType),
+    m_description(std::move(description)),
+    m_eventTime(DateTime::GetCurrentDateTime())
+  {
+  }
+
+  Event::EventType Event::GetEventType() const
+  {
+    return m_eventType;
+  }
+
+  time_t Event::GetEventTime() const
+  {
+    return m_eventTime;
+  }
+
+  std::string Event::ToString() const
+  {
+    return "EventId: " +
+      to_string(m_eventType) +
+      " Description: " +
+      m_description;
+  }
+}
---
+++ RXEventAggregator/Misc/DateTime.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <chrono>
+
+namespace Misc
+{
+  class DateTime
+  {
+  public:
+    DateTime(const DateTime& other) = delete;
+    DateTime(DateTime&& other) noexcept = delete;
+    DateTime& operator=(const DateTime& other) = delete;
+    DateTime& operator=(DateTime&& other) noexcept = delete;
+    static time_t GetCurrentDateTime()
+    {
+      auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+      return now;
+    }
+  };
+}
